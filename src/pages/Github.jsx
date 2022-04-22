22fd48198cce2196a14d22d3bae60bc49c6fe048
Leonardo Amaral <leozero103@gmail.com>
2022-04-22 16:37:35 -0300

    .

--- src/pages/Github.jsx
+++ src/pages/Github.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import GitHubAPI from '../services/GitHubAPI'
-import '../styles/GithubStyle/GithubAPI.css'
+import '../styles/GithubStyle/Github.css'
 
 function Github() {
   return (
     <div className='Github'>
       <Nav />
+      {/* <GitHubPerfil /> */}
       <GitHubAPI />
     </div>
   )
--- src/services/GitHubAPI.jsx
+++ src/services/GitHubAPI.jsx
@@ -15,21 +15,22 @@ function GitHubAPI() {
 
   return (
     <div className='GitHubAPI'>
-      <div className="boxes-api">
-        <div className="box">
-          {
-            repositories.map((repositories) => {
-              return (
+      <div className="box-api">
+        {
+          repositories.map((repositories) => {
+            return (
+              <div className="repo-box">
                 <div className="box-items"
                   key={repositories.name}
                 >
                   <h1>{repositories.name}</h1>
                   <h3>{repositories.language}</h3>
+                  <p>{repositories.description}</p>
                 </div>
-              )
-            })
-          }
-        </div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
