193f2c616a1c2778f887a73c878895e20aeb33d7
Leonardo Amaral <leozero103@gmail.com>
2022-04-26 11:37:17 -0300

    Alert

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Content from './pages/Content';
 import Github from './pages/Github';
 
 function App() {
+  alert('O portifolio ainda está em desenvolvimento, então é normal encontrar alguns bugs, ou algumas falhas.')
   return (
     <div className="App">
       <Introduction />
