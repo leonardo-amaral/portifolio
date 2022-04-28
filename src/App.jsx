06374a8889fdd17b3904d7c2ae0cd64249cebbbf
Leonardo Amaral <leozero103@gmail.com>
2022-04-28 16:49:46 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './styles/App.css'
 import Github from './pages/Github';
 import Home from './pages/Home';
-import { BrowserRouter, Route, Routes, Router } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   // alert('O portifolio ainda está em desenvolvimento, então é normal encontrar alguns bugs, ou algumas falhas.')
