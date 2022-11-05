61715beff223e9daa420ecc76753ca75d41b566b
leonardo-amaral <leozero103@gmail.com>
2022-11-05 17:32:36 -0300

    123

--- src/components/subComponents/Repositories.jsx
+++ src/components/subComponents/Repositories.jsx
@@ -35,15 +35,15 @@ export function Repositories (){
         {
           repositories?.map((repositories) => {
             return (
-          <div className="repo-box" data-aos="fade-up" key={repositories.name}
+          <div className="repo-box" data-aos="fade-up" key={repositories?.name}
           >
-          <a href={repositories.html_url} target="new_blank">
-          <Text color='white' fontSize='24px' fontWeight='800' textTransform='lowerrcase' h='6vh' w='100%' backgroundColor='rgb(40, 40, 125)' textAlign='center' paddingTop='15px'>{repositories.name}</Text>
+          <a href={repositories?.html_url} target="new_blank">
+          <Text color='white' fontSize='24px' fontWeight='800' textTransform='lowerrcase' h='6vh' w='100%' backgroundColor='rgb(40, 40, 125)' textAlign='center' paddingTop='15px'>{repositories?.name}</Text>
           <Box bg='black' overflow='hidden'>
-          <img className='image-repo-git' src={repositories.image} alt='preview repo' />
+          <img className='image-repo-git' src={repositories?.image} alt='preview repo' />
           </Box>
             <div className="box-items">
-              <Text w='100%' paddingTop='10px' textAlign='center' fontSize='20px' fontWeight='800' bg='rgba(255, 255, 255, 0.4)' h='4vh' color='rgb(40, 40, 125)'>{repositories.language}</Text>
+              <Text w='100%' paddingTop='10px' textAlign='center' fontSize='20px' fontWeight='800' bg='rgba(255, 255, 255, 0.4)' h='4vh' color='rgb(40, 40, 125)'>{repositories?.language}</Text>
               <Text fontSize='16px' margin='2%' color="rgba(0,0,0, 0.8)">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</Text>          
             </div>
             </a>
